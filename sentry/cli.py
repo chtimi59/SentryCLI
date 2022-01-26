06627ab5ac61bba8c433e9438d69b2d747b1e01e
chtimi59 <jdorgeville@gmail.com>
2022-01-26 10:17:47 +0100

    add delete_issues and get_tags

--- sentry/cli.py
+++ sentry/cli.py
@@ -2,13 +2,19 @@ import os
 import re
 import sys
 import urllib.parse
-from typing import List
+from typing import Dict, List
 
 import dotenv
 import requests
 from pydantic import BaseModel
 
 dotenv.load_dotenv()
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 
 def error(msg: str):
@@ -27,6 +33,15 @@ class Issues(BaseModel):
     __root__: List[Issue] = []
 
 
+class Tag(BaseModel):
+    value: str
+    count: str
+
+
+class Tags(BaseModel):
+    __root__: List[Tag] = []
+
+
 ORGANIZATION_SLUG = os.getenv("ORGANIZATION_SLUG")
 if ORGANIZATION_SLUG is None:
     error("ORGANIZATION_SLUG is missing !")
@@ -36,12 +51,12 @@ if PROJECT_SLUG is None:
 SENTRY_TOKEN = os.getenv("SENTRY_TOKEN")
 if SENTRY_TOKEN is None:
     error("SENTRY_TOKEN is missing !")
-URL = f"https://sentry.io/api/0/projects/{ORGANIZATION_SLUG}/{PROJECT_SLUG}/"
+URL_API0 = "https://sentry.io/api/0/"
+URL_PROJECTS = URL_API0 + f"projects/{ORGANIZATION_SLUG}/{PROJECT_SLUG}/"
 
 
-# For pagination
-# https://docs.sentry.io/api/pagination/
-def getNextUrl(link: str):
+def next_url(link: str):
+    """Sentry Pagination : https://docs.sentry.io/api/pagination/"""
     m = re.search(
         "^"
         + '<(?P<url1>.+)>; rel="(?P<rel1>.+)"; results="(?P<results1>.+)"; cursor="(?P<cursor1>.+)'
@@ -54,25 +69,61 @@ def getNextUrl(link: str):
     return m.group(f"url{idx}") if m.group(f"results{idx}") == "true" else None
 
 
-def getIssues(query: str = "is:unresolved", page: int = 1) -> List[Issue]:
+# GET /api/0/projects/{organization_slug}/{project_slug}/issues/
+def get_issues(query: str = "is:unresolved", page: int = 1) -> List[Issue]:
     result: List[Issue] = []
-    url = URL + "issues/?" + urllib.parse.urlencode({"query": query})
+    url = URL_PROJECTS + "issues/?" + urllib.parse.urlencode({"query": query})
     while url is not None and page > 0:
         r = requests.get(url, headers={"Authorization": f"Bearer {SENTRY_TOKEN}"})
         if not r.status_code == 200:
             error(f"{url} returns HTTP {r.status_code}")
-        url = getNextUrl(r.headers["link"])
-        result.extend(Issues.parse_obj(r.json()).__root__)
+        url = next_url(r.headers["link"])
+        payload = r.json()
+        result.extend(Issues.parse_obj(payload).__root__)
         page -= 1
     return result
 
 
+# GET /api/0/issues/{issue_id}/tags/{key}/values/
+def get_tags(issue_id: int, tag_name: str, page: int = 1) -> Dict[str, int]:
+    result: Dict[str, int] = {}
+    url = URL_API0 + f"issues/{issue_id}/tags/{tag_name}/values/"
+    while url is not None and page > 0:
+        r = requests.get(url, headers={"Authorization": f"Bearer {SENTRY_TOKEN}"})
+        if not r.status_code == 200:
+            error(f"{url} returns HTTP {r.status_code}")
+        url = next_url(r.headers["link"])
+        payload = r.json()
+        for tag in Tags.parse_obj(payload).__root__:
+            result[tag.value] = tag.count
+        page -= 1
+    return result
+
+
+# DELETE /api/0/projects/{organization_slug}/{project_slug}/issues/
+def delete_issues(ids: List[int]):
+    url = URL_PROJECTS + "issues/?" + "&".join(map(lambda id: f"id={id}", ids))
+    r = requests.delete(url, headers={"Authorization": f"Bearer {SENTRY_TOKEN}"})
+    if not r.status_code == 204:
+        error(f"{url} returns HTTP {r.status_code}")
+    print(f"deleted: {ids}")
+
+
+def delete_from_query(query: str, count=1):
+    while count > 0:
+        # 5 pages of 100 entries
+        issues = get_issues(page=5, query=query)
+        for issue in issues:
+            print(issue.title)
+        # bullk delete (10)
+        ids = list(map(lambda issue: issue.id, issues))
+        for ids in chunks(ids, 10):
+            delete_issues(ids)
+        count -= 1
+
+
 def main() -> None:
-    data = getIssues(page=2)
-    # "is:unresolved release:2201.6.0")
-    print(f"size = {len(data)}")
-    # for p in data:
-    #    print(p.title)
+    delete_from_query("is:unresolved release:kd-dem@2201.5.0", count=10)
 
 
 if __name__ == "__main__":
